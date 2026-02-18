10bf36b0ca90a7d5095f5b2dd4b1a7dfdadb388e
sauravkumar0723 <98612833+sauravkumar0723@users.noreply.github.com>
2026-02-18 12:41:32 +0530

    Add files via upload

---
+++ DOM/script.js
@@ -0,0 +1,29 @@
+// console.log(alert("hello, keep more practice in js"));
+// Dom manipulation
+// 1. selecting with id 
+let header  = document.getElementById("my-id") // target the h1 tag
+console.log(header);
+
+//  selecting with class
+// doucument.getElementsByClassNAme("myClass")
+
+let headings = document.getElementsByClassName("heading");
+console.log(headings);
+
+3. // selecting wtih tag
+document.getElementsByTagName
+let parahs = document.getElementsByTagName("p");
+console.log(parahs);
+
+//4. query Selector
+// returns the first element
+// documents.querySelector("myId / my Class / tag")
+let elements = document.querySelector("p");
+console.log(firstEl);
+
+// 5.
+let secondElements = document.querySelectorAll("p");
+console.log(secondElements);
+
+// DOM Propertites
+
